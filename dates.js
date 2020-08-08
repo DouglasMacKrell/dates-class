655ac67fd943fcbc104286a019140a9f546e40c7
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-08-08 11:59:03 -0400

    Date Test

---
+++ dates.js
@@ -0,0 +1,9 @@
+// Find what is the oldest date you can represent in JavaScript.
+// Find what is the largest(futuristic) date you can represent in JavaScript.
+// If you have siblings calculate the numbers of day between your birthday and theirs.
+// What is the result of new Date(0)? By what name is this date known?
+// What is your birth date in ISO8601 format?
+
+let dougBday = new Date("1980-05-28T06:04:00.553Z");
+
+console.log(dougBday.toString())
